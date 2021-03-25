975fa3321e9122b98fff2023b0bb431a990d7e1d
Almer <48512743+meisterAlmer@users.noreply.github.com>
2021-03-25 20:59:29 +0100

    Logo added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import FruitList from "./components/FruitList";
 import Form from "./components/Form";
+import logo from "./assets/screenshot-logo.png";
 
 function App() {
   return (
     <>
+      <img src={logo} alt="logo" className="logo-img" />
       <FruitList></FruitList>
       <Form></Form>
     </>
